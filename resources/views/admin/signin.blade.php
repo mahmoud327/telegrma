9514e072a9ead177cb89c4850b637c6a0a25b6dd
mahmoudmohamed <mahmoudmohamed199885.com>
2023-02-18 16:10:18 +0200

    D

--- resources/views/admin/signin.blade.php
+++ resources/views/admin/signin.blade.php
@@ -1,46 +1,50 @@
 @extends('admin.layouts.master2')
 @section('css')
-<!-- Sidemenu-respoansive-tabs css -->
-<link href="{{URL::asset('assets/plugins/sidemenu-responsive-tabs/css/sidemenu-responsive-tabs.css')}}" rel="stylesheet">
+    <!-- Sidemenu-respoansive-tabs css -->
+    <link href="{{ URL::asset('assets/plugins/sidemenu-responsive-tabs/css/sidemenu-responsive-tabs.css') }}"
+        rel="stylesheet">
 @endsection
 @section('content')
-		<div class="container-fluid">
-			<div class="row no-gutter">
-				<!-- The image half -->
-				<div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
-					<div class="row wd-100p mx-auto text-center">
-						<div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
-							<img src="{{asset('website/img/logo/logo.png')}}" class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
-						</div>
-					</div>
-				</div>
-				<!-- The content half -->
-				<div class="col-md-6 col-lg-6 col-xl-5 bg-white">
-					<div class="login d-flex align-items-center py-2">
-						<!-- Demo content-->
-						<div class="container p-0">
-							<div class="row">
-								<div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
+    <div class="container-fluid">
+        <div class="row no-gutter">
+            <!-- The image half -->
+            <div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
+                <div class="row wd-100p mx-auto text-center">
+                    <div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
+                        <img src="{{ asset('website/img/logo/logo.png') }}"
+                            class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
+                    </div>
+                </div>
+            </div>
+            <!-- The content half -->
+            <div class="col-md-6 col-lg-6 col-xl-5 bg-white">
+                <div class="login d-flex align-items-center py-2">
+                    <!-- Demo content-->
+                    <div class="container p-0">
+                        <div class="row">
+                            <div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
 
-									<div class="card-sigin">
-										<div class="card-sigin">
-											<div class="main-signup-header">
-												<div class="card-body text-danger">
-                                                    <ul class="list-unstyled">
-                                                        @foreach ($errors->all() as $error)
-                                                            <li class="alert alert-danger">{{ $error }}</li>
-                                                        @endforeach
-                                                    </ul>
+                                <div class="card-sigin">
+                                    <div class="card-sigin">
+                                        <div class="main-signup-header">
+                                            <div class="card-body text-danger">
+                                                <ul class="list-unstyled">
+                                                    @foreach ($errors->all() as $error)
+                                                        <li class="alert alert-danger">{{ $error }}</li>
+                                                    @endforeach
+                                                </ul>
+                                            </div>
+                                            <form action="{{ route('admin.login') }}" method="POST">
+                                                @csrf
+                                                <div class="form-group">
+                                                    <label>Email</label> <input class="form-control" name="email"
+                                                        placeholder="Enter your email" type="text">
                                                 </div>
-												<form action="{{ route('admin.login') }}" method="POST">
-                                                    @csrf
-													<div class="form-group">
-														<label>Email</label> <input class="form-control" name="email" placeholder="Enter your email" type="text">
-													</div>
-													<div class="form-group">
-														<label>Password</label> <input class="form-control" name="password" placeholder="Enter your password" type="password">
-													</div><button class="btn btn-main-primary btn-block">Sign In</button>
-													{{-- <div class="row row-xs">
+                                                <div class="form-group">
+                                                    <label>Password</label> <input class="form-control" name="password"
+                                                        placeholder="Enter your password" type="password">
+                                                </div><button class="btn btn-main-primary btn-block">Sign In</button>
+                                                {{-- <div class="row row-xs">
 														<div class="col-sm-6">
 															<button class="btn btn-block"><i class="fab fa-facebook-f"></i> Signup with Facebook</button>
 														</div>
@@ -48,21 +52,21 @@
 															<button class="btn btn-info btn-block"><i class="fab fa-twitter"></i> Signup with Twitter</button>
 														</div>
 													</div> --}}
-												</form>
-												{{-- <div class="main-signin-footer mt-5">
+                                            </form>
+                                            {{-- <div class="main-signin-footer mt-5">
 													<p><a href="">Forgot password?</a></p>
 													<p>Don't have an account? <a href="{{ url('/' . $page='signup') }}">Create an Account</a></p>
 												</div> --}}
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div><!-- End -->
-					</div>
-				</div><!-- End -->
-			</div>
-		</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- End -->
+                </div>
+            </div><!-- End -->
+        </div>
+    </div>
 @endsection
 @section('js')
 @endsection
