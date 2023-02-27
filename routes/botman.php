44a2cf78d41ebb6e8e81d96a49c3b8eb12b897d6
mahmoudmohamed <mahmoudmohamed199885.com>
2023-02-28 00:09:02 +0200

    g

--- routes/botman.php
+++ routes/botman.php
@@ -35,7 +35,9 @@ $botman->hears('Hi', function (BotMan $bot) {
 });
 
 $botman->hears('start', function (BotMan $bot) {
-    $bot->startConversation(new QuizConversation());
-})->stopsConversation();
+    // $bot->startConversation(new QuizConversation());
+    $bot->reply('Hello! write start for begin exam');
+
+});
 
 $botman->listen();
